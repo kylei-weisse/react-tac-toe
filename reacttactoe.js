c4c1315ce18faa5df2167e297773ba1cf365f6fb
Kyle Weisse <kyle.weisse@gmail.com>
2021-04-15 11:53:04 -0400

    added constructor to Game class

--- reacttactoe.js
+++ reacttactoe.js
@@ -69,6 +69,15 @@ function Square(props) {
   }
   
   class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+          history: [{
+            squares: Array(9).fill(null),
+          }],
+          xIsNext: true,
+        };
+    }
     render() {
       return (
         <div className="game">
