8e72360ac76a21c7edb425aa20cff5944ae96146
Kyle Weisse <kyle.weisse@gmail.com>
2021-04-15 15:31:55 -0400

    added a slice to history inside of handleClick

--- reacttactoe.js
+++ reacttactoe.js
@@ -54,7 +54,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -65,6 +65,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
