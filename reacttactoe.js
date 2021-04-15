f77cf3969c57944fa1a77ab2d32bb716c00d0a59
Kyle Weisse <kyle.weisse@gmail.com>
2021-04-15 15:15:38 -0400

    added history

--- reacttactoe.js
+++ reacttactoe.js
@@ -72,6 +72,18 @@ function Square(props) {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+      
+      const moves = history.map((step, move) => {
+        const desc = move ?
+              'Go to move #' + move : 
+              'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={()=> this.jumpTo(move)}>{desc}</button>
+          </li>
+        )
+      });
+      
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -89,7 +101,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
