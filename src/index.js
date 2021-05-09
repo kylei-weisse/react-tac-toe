3d886be447eedd2c9ee11ba7967b625e7d09ec44
Kyle Weisse <kyle.weisse@gmail.com>
2021-05-09 08:38:15 -0400

    winner function console logging

--- src/index.js
+++ src/index.js
@@ -159,8 +159,10 @@ function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      console.log("squares[a]", squares[a])
+    console.log("squares[a]: ", squares[a], "lines[i]: ", lines[i]);
+    if (squares[a] && 
+      squares[a] === squares[b] && 
+      squares[a] === squares[c]) {
       return squares[a];
     }
   }
