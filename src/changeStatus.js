1b8573fdf2b9a4c3bb8c811e6afe89032d3ba655
Kyle Weisse <kyle.weisse@gmail.com>
2021-05-26 13:56:33 -0400

    cleaned up comments

--- src/changeStatus.js
+++ src/changeStatus.js
@@ -2,17 +2,15 @@
 //this is not a React component, but a simple
 //JS function.
 function changeStatus(state){
-    //to do: link the new showStatus component to the rest of the app
     let status;
-    //check for winner
     if (state.winner) {
       status = 'Winner: ' + state.winner;
       console.log(state.winningLine);
-      //else, check that there's more moves left.
+      //only show next player if there's still moves left to make
     } else if (state.stepNumber <= 8) {
       status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
-      //else, draw
     } else {
+      //draw
       status = 'The Game has Ended in a Draw';
     }
     return status;
