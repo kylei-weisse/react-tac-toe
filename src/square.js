e4ae894383ff3866f4af7c6b15e3aa9e501a2863
Kyle Weisse <kyle.weisse@gmail.com>
2021-05-28 11:48:09 -0400

    board passes className to Square

--- src/square.js
+++ src/square.js
@@ -1,6 +1,6 @@
 function Square(props) {
     return (
-      <button className="square" onClick={props.onClick}>
+      <button className={props.className} onClick={props.onClick}>
         {props.value}
       </button>
     );
